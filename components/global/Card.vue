6c65b52c0d152487173d9385f278361aaf312135
Nizar Ahmad Baihaqi <nizarbaihaq@gmail.com>
2021-11-30 17:31:31 +0700

    Remove unnecessary class🎨

--- components/global/Card.vue
+++ components/global/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:rounded-lg bg-pastel-cream dark:bg-night-purple shadow-xl dark:shadow-none flex my-3 overflow-hidden w-full">
+  <div class="md:rounded-lg bg-pastel-cream dark:bg-night-purple shadow-xl flex my-3 overflow-hidden w-full">
     <div class="py-7 px-7 w-full">
       <slot />
     </div>
